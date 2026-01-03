a069c4f0e091e8fd49ab8d4c008e44f8528f2e55
Dylan Wells <dylan@dylanjwells.com>
2026-01-03 12:53:46 -0500

    Update config.ts

--- src/blocks/RichTextBlock/config.ts
+++ src/blocks/RichTextBlock/config.ts
@@ -1,21 +1,8 @@
 import type { Block } from 'payload'
 
 import {
-  BlockquoteFeature,
-  BoldFeature,
-  ChecklistFeature,
   FixedToolbarFeature,
   HeadingFeature,
-  HorizontalRuleFeature,
-  IndentFeature,
-  InlineToolbarFeature,
-  ItalicFeature,
-  LinkFeature,
-  OrderedListFeature,
-  ParagraphFeature,
-  StrikethroughFeature,
-  UnderlineFeature,
-  UnorderedListFeature,
   lexicalEditor,
 } from '@payloadcms/richtext-lexical'
 
@@ -35,21 +22,7 @@ export const RichTextBlock: Block = {
         features: ({ defaultFeatures }) => [
           ...defaultFeatures,
           HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }),
-          BoldFeature(),
-          ItalicFeature(),
-          UnderlineFeature(),
-          StrikethroughFeature(),
-          OrderedListFeature(),
-          UnorderedListFeature(),
-          ChecklistFeature(),
-          BlockquoteFeature(),
-          HorizontalRuleFeature(),
-          IndentFeature(),
-          LinkFeature({
-            enabledCollections: ['pages', 'projects', 'works'],
-          }),
           FixedToolbarFeature(),
-          InlineToolbarFeature(),
         ],
       }),
     },
