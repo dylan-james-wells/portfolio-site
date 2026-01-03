625dd76681b6e281ea186e52302503cecece9d8a
Dylan Wells <dylan@dylanjwells.com>
2026-01-03 13:57:40 -0500

    Update config.ts

--- src/blocks/RichTextBlock/config.ts
+++ src/blocks/RichTextBlock/config.ts
@@ -3,11 +3,6 @@ import type { Block } from 'payload'
 import {
   lexicalEditor,
   HeadingFeature,
-  UnorderedListFeature,
-  OrderedListFeature,
-  ChecklistFeature,
-  BlockquoteFeature,
-  HorizontalRuleFeature,
   FixedToolbarFeature,
   InlineToolbarFeature,
 } from '@payloadcms/richtext-lexical'
@@ -25,14 +20,9 @@ export const RichTextBlock: Block = {
       type: 'richText',
       required: true,
       editor: lexicalEditor({
-        features: ({ rootFeatures }) => [
-          ...rootFeatures,
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
           HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }),
-          UnorderedListFeature(),
-          OrderedListFeature(),
-          ChecklistFeature(),
-          BlockquoteFeature(),
-          HorizontalRuleFeature(),
           FixedToolbarFeature(),
           InlineToolbarFeature(),
         ],
