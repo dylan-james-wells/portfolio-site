331365ede57aca84cbb7cbaf3252a06648cb2ca3
Dylan Wells <dylan@dylanjwells.com>
2026-01-09 19:55:53 -0500

    Update mergeOpenGraph.ts

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -9,8 +9,8 @@ const defaultOpenGraph: Metadata['openGraph'] = {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Dylan Wells',
-  title: 'Dylan Wells',
+  siteName: 'Dylan Wells - Web Developer',
+  title: 'Dylan Wells - Web Developer',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
